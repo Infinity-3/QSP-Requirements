484e11c145b38d138696e81679622979306cfed4
Prajwal02shekar <Prajwal02shekar@gmail.com>
2025-04-11 16:11:14 +0530

    #

--- qspiders/settings.py
+++ qspiders/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a4nz*vs2ix7ea^993f@xeah2q+-1slkqgwg1uchowd5y$h2u^-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','vercel.app','localhost','.vercel.app']
 
